f79871db1d925084c5436db3c5eacd09c4560ce1
glen zhang <glenzhang@live.com>
2014-12-05 12:47:52 +0800

    add hover tips

--- js/magicbar.js
+++ js/magicbar.js
@@ -2,49 +2,62 @@
 define(["jquery", "loader", "statemachine"], function($, L, StateMachine) {
     var isIe6 = !-[1, ] && !window.XMLHttpRequest;
     var $window = $(window);
+    var $document = $(document);
+    var triggerSelector = ".J_mb_trigger";
+    var tipsSelector = ".J_mb_tips";
+    var mouseenterSID;
+
+    var stateManager = new StateMachine();
 
     function init($container) {
-        var moduleSB = new StringBuilder();
-        var stateManager = new StateMachine();
-        
         setTimeout(function() {
             $container.css({
                 right: 0
             });
         }, 25);
 
-        $(document).on("click", ".J_mb_trigger", function(ev) {
-            ev.preventDefault();
-            var $trigger = $(this);
-            var moduleName = $trigger.data("module");
-            var currentController = stateManager.controllerObj[moduleName];
-            var hasLoaded = currentController != undefined;
+        $(triggerSelector, $container).on("mouseenter", mouseenterHandler)
+            .on("mouseleave", mouseleaveHandler)
+            .on("click", function(ev) {
+                ev.preventDefault();
 
-            if (hasLoaded) {
-                if (currentController.$trigger[0] == $trigger[0]) {
-                    currentController.toggle();
-                } else {
-                    currentController.$trigger = $trigger;
-                    currentController.active();
-                }
+                var $trigger = $(this);
+                var moduleName = $trigger.data("module");
+                var currentController = stateManager.controllerObj[moduleName];
+                var hasLoaded = currentController != undefined;
 
-            } else {
-                L.getHtml("/partial/{0}.html".format(moduleName), function(res) {
-                    var $moduleTemplate = $(res).appendTo($container);
-
-                    require(["{0}module".format(moduleName)], function(Module) {
-                        var module = new Module();
-
-                        module.$trigger = $trigger;
-                        module.$view = $moduleTemplate;
-                        module.name = moduleName;
-                        stateManager.add(module);
-                        $trigger.data("name", module.name);
-                        module.setup();
-                    });
+                $trigger.find(tipsSelector).css({
+                    right: "50px",
+                    opacity: 0
                 });
-            }
-        }).on("click", function(ev) {
+
+                if (hasLoaded) {
+                    if (currentController.$trigger[0] == $trigger[0]) {
+                        currentController.toggle();
+                    } else {
+                        currentController.$trigger = $trigger;
+                        currentController.active();
+                    }
+
+                } else {
+                    L.getHtml("/partial/{0}.html".format(moduleName), function(res) {
+                        var $moduleTemplate = $(res).appendTo($container);
+
+                        require(["{0}module".format(moduleName)], function(Module) {
+                            var module = new Module();
+
+                            module.$trigger = $trigger;
+                            module.$view = $moduleTemplate;
+                            module.name = moduleName;
+                            stateManager.add(module);
+                            $trigger.data("name", module.name);
+                            module.setup();
+                        });
+                    });
+                }
+            });
+
+        $document.on("click", function(ev) {
             var $this = $(ev.target);
             if ($this.parents(".J_mb_container").length == 0) {
                 stateManager.deactiveAll();
@@ -54,12 +67,42 @@ define(["jquery", "loader", "statemachine"], function($, L, StateMachine) {
         $window.on("resize.MB", function() {
             stateManager.deactiveAll();
         });
-        
-        if(isIe6) {
+
+        if (isIe6) {
             $window.on("scroll.MB", function() {
-                $container.css({'top': $window.scrollTop()});
+                $container.css({
+                    'top': $window.scrollTop()
+                });
             });
         }
+    }
+
+    function mouseenterHandler() {
+        var $this = $(this);
+        var moduleName = $this.data("module");
+        var currentController = stateManager.controllerObj[moduleName];
+
+        if(currentController && currentController.showing) {
+            return;
+        }
+
+        mouseenterSID = setTimeout(function() {
+            $this.find(tipsSelector).show().animate({
+                right: "33px",
+                opacity: 1
+            }, 150);
+        }, 250);
+    }
+
+    function mouseleaveHandler() {
+        clearTimeout(mouseenterSID);
+
+        $(this).find(tipsSelector).animate({
+            right: "50px",
+            opacity: 0
+        }, 150, function() {
+            $(this).hide();
+        });
     }
 
     return {
--- js/modulebase.js
+++ js/modulebase.js
@@ -4,11 +4,13 @@ define(["jquery"], function($) {
     var ModuleBase = function() {
         this.$view = "";
         this.$trigger = "";
+        /*
         this.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random() * 16 | 0,
                 v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         }).toUpperCase();
+        */
         this.name = "";
     };
 
